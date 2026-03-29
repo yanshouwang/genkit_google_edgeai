99823dcd4f0a9a57589c7e4d15160632400cda53
yanshouwang <yanshouwang@zeekr.dev>
2026-03-29 11:14:28 +0800

    创建项目

---
+++ example/genkit_google_edgeai_example.dart
@@ -0,0 +1,6 @@
+import 'package:genkit_google_edgeai/genkit_google_edgeai.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/genkit_google_edgeai_test.dart
@@ -0,0 +1,16 @@
+import 'package:genkit_google_edgeai/genkit_google_edgeai.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
